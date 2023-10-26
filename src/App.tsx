f6b1871502cf75f5b58e5d5ac98580e9b6668722
Moe <masihdon@hotmail.de>
2023-10-26 21:10:53 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Typography variant='h1'>
+        Consent Form
+      </Typography>
+    </>
+  )
+}
+
+export default App
