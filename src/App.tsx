4e309084f5d55aaa73f03471289fadb5ec165cbf
Moe <masihdon@hotmail.de>
2023-10-28 22:15:21 +0200

    create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Typography } from '@mui/material';
+
 import './App.css'
+import Header from './pages/Header';
+
 
 function App() {
 
 
   return (
     <>
+      <Header />
       <Typography variant='h1'>
         Consent Form
       </Typography>
