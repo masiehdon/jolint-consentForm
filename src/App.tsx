3f5ca1a387542eb939e1da4a1a0fdb6706f5a8fb
Moe <masihdon@hotmail.de>
2023-10-29 14:47:23 +0100

    intialising branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Typography } from '@mui/material';
-
-import './App.css'
 import Header from './pages/Header';
+import Footer from './pages/Footer';
+import './App.css'
+
 
 
 function App() {
@@ -10,9 +10,7 @@ function App() {
   return (
     <>
       <Header />
-      <Typography variant='h1'>
-        Consent Form
-      </Typography>
+      <Footer  />
     </>
   )
 }
