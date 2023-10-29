ddcce7fd2f1f30f3d9e4978a10ead95d1bd7b117
Moe <masihdon@hotmail.de>
2023-10-29 17:54:34 +0100

    03-consent form/ Footer

--- src/pages/Footer/index.tsx
+++ src/pages/Footer/index.tsx
@@ -8,26 +8,26 @@ const bgColor = '#2C3386'
 export default function Footer() {
     return (
      <footer>
-      
-            <Grid container sx={{ background: bgColor, color: 'white', textAlign: 'center', position: 'absolute', bottom: 0, padding: 3, alignItems: 'center'  }}>
-              <Grid item xs={4} sx={{display: 'inline', textAlign: 'right',}}>
-                  <Typography variant="body2" sx={{ lineHeight: 2, mr: 35}}>
+    
+            <Grid container sx={{ background: bgColor, color: 'white', textAlign: 'center', position: 'absolute', bottom: 0, alignItems: 'center', minWidth: '100vw', display: { xs: 'none', sm: 'flex' } }}>
+              <Grid item  md={6} sx={{textAlign: 'right', mt:2}}>
+                  <Typography variant="body2" sx={{ lineHeight: 2, mr: 60}}>
                      Jolint Network Analytics AB <br/>Org. Nr. 559324-1770<br/> Gothenburg, Sweden 
                   </Typography>
                 </Grid>
-                <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'center'}}>
+                
+                <Grid item md={6}  sx={{textAlign: 'left',  mt:2}}>
+                    
+                  <Typography variant="body2" sx={{lineHeight: 2, ml: 60, }}>
+                     <LinkedInIcon/> Jolint <br/><MailOutlineOutlinedIcon /> hello@jolint.com
+                  </Typography>
+              </Grid>
+         <Grid item  md={12} sx={{ display: 'flex', justifyContent: 'center', mb: 1 }}>
                   <Typography variant="body2">
                      Copyright © 2023 Jolint | Powered by Jolint
  
                   </Typography>
               </Grid>
-                <Grid item xs={4} sx={{textAlign: 'left', alignContent: 'center'}}>
-                    
-                  <Typography variant="body2" sx={{lineHeight: 2, ml: 35}}>
-                     <LinkedInIcon/> Jolint <br/><MailOutlineOutlinedIcon /> hello@jolint.com
-                  </Typography>
-              </Grid>
-         
           </Grid>
               
        </footer>
