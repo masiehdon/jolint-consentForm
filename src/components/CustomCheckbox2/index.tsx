41448283fde885885c69d8a0fefb092c8c425a1c
Moe <masihdon@hotmail.de>
2023-11-05 21:33:59 +0100

    18-consent form/Checkboxes inside boxes

---
+++ src/components/CustomCheckbox2/index.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@mui/material';
+
+
+export default function Checkboxes() {
+  return (
+    <Box sx={{ width: 30, height:30, border: '.5px solid black', }}>
+      
+    </Box>
+      
+  );
+}
