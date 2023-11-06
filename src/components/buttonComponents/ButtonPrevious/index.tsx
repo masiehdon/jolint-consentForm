7a546c5120ecb9855d1dc45181e05e0db0136b1a
Moe <masihdon@hotmail.de>
2023-11-06 09:57:05 +0100

    21-consent form/Button Previous

---
+++ src/components/buttonComponents/ButtonPrevious/index.tsx
@@ -0,0 +1,24 @@
+import { Button, Box } from "@mui/material";
+
+interface PrevProp {
+  handlePreviousPage:() => void
+}
+
+export default function index({ handlePreviousPage }: PrevProp) {
+  return (
+    
+          <Box sx={{m: 5}}>
+         
+      <Button
+        onClick={handlePreviousPage}
+        variant="outlined"
+        size="large"
+          sx={{
+            border: '2px solid #2C3386', width: 175, height: 55
+          }}
+      >
+          Back
+        </Button>
+       </Box>
+     )
+}
